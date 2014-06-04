8a97afaec1ea2bc876032cb36ebe9161c1157c4b
David J. Brenes <david.brenes@simplelogica.net>
2014-06-04 16:39:35 +0200

    Testing that no draft items are shown

--- spec/requests/no_cms/news/item_specs.rb
+++ spec/requests/no_cms/news/item_specs.rb
@@ -5,9 +5,11 @@ describe NoCms::News::Item do
   context "when visiting item index" do
 
     let(:news_items) { create_list :nocms_news_item, 3 }
+    let(:draft_news_items) { create_list :nocms_news_item, 3, draft: true }
 
     before do
       news_items
+      draft_news_items
       visit no_cms_news.items_path
     end
 
@@ -15,6 +17,12 @@ describe NoCms::News::Item do
     it("should display news items") do
       news_items.each do |item|
         expect(page).to have_selector('h2', text: item.title)
+      end
+    end
+
+    it("should not display drafts") do
+      draft_news_items.each do |item|
+        expect(page).to_not have_selector('h2', text: item.title)
       end
     end
 
