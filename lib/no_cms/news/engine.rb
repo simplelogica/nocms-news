991d5bcc771c26047d7a07ab3313c7c2bef27e04
David J. Brenes <david.brenes@simplelogica.net>
2014-06-05 18:22:15 +0200

    Kaminari gem installed

--- lib/no_cms/news/engine.rb
+++ lib/no_cms/news/engine.rb
@@ -1,6 +1,8 @@
 require 'globalize'
+require 'kaminari'
 require 'friendly_id'
 require "friendly_id/globalize"
+
 module NoCms::News
   class Engine < ::Rails::Engine
     isolate_namespace NoCms::News
