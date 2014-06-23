dd78cca36f9a562e559062d142446f65f2a89a22
David J. Brenes <david.brenes@simplelogica.net>
2014-06-23 15:35:31 +0200

    Explictly pointing which classes to use in the HABTM relationship

--- app/models/no_cms/news/category.rb
+++ app/models/no_cms/news/category.rb
@@ -2,7 +2,7 @@ module NoCms::News
   class Category < ActiveRecord::Base
     translates :title
 
-    has_and_belongs_to_many :items
+    has_and_belongs_to_many :items, class_name: 'NoCms::News::Item'
 
     validates :title, presence: true
   end
