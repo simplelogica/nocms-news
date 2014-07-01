8e144ee42eea64b51ec85adbe3f8b7ae78a66e17
David J. Brenes <david.brenes@simplelogica.net>
2014-07-01 11:37:52 +0200

    Nesting modules or the gem can't be properly installed in a new project

--- lib/no_cms/news/engine.rb
+++ lib/no_cms/news/engine.rb
@@ -3,8 +3,10 @@ require 'kaminari'
 require 'friendly_id'
 require "friendly_id/globalize"
 
-module NoCms::News
-  class Engine < ::Rails::Engine
-    isolate_namespace NoCms::News
+module NoCms
+  module News
+    class Engine < ::Rails::Engine
+      isolate_namespace NoCms::News
+    end
   end
 end
