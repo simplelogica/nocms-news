97a8920aea41a0d06fbba1920f7c7fb30657019b
David J. Brenes <david.brenes@simplelogica.net>
2014-07-10 17:23:50 +0200

    We must order the news by the date

--- app/controllers/no_cms/news/items_controller.rb
+++ app/controllers/no_cms/news/items_controller.rb
@@ -6,7 +6,7 @@ module NoCms
         @items_page = params[:page].to_i
         @items_page = 1 if @items_page < 1
         @items_page_size = 10
-        @items = Item.includes(:translations).no_drafts.page(@items_page).per(@items_page_size)
+        @items = Item.includes(:translations).no_drafts.page(@items_page).with_translations(I18n.locale).order("#{Item::Translation.table_name}.published_at DESC").per(@items_page_size)
       end
 
       def show
